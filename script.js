e92c271b97a57fdfb6a3eb7b4ff51e4df5bf44c2
Bomtara <geassiscool@gmail.com>
2025-05-18 18:47:47 +0530

    woo hooo

---
+++ script.js
@@ -0,0 +1,28 @@
+const box = document.querySelector(".box");
+const fragment = new DocumentFragment();
+const n = 10;
+const p = 100/n;
+
+function handleHoverEnter(event){
+    const hoveredDiv = event.target;
+    const randc = Math.floor(Math.random()*16777215);
+    let randcolor = randc.toString(16);
+    let hoverCount = parseInt(hoveredDiv.dataset.hoverCount || '0', 10);
+    if(hoverCount<10){
+        hoveredDiv.style.backgroundColor = "#"+randcolor ;
+        let opac = (hoverCount+1)*10;
+        hoveredDiv.style.opacity = opac.toString() + "%";
+        hoverCount++;
+        hoveredDiv.dataset.hoverCount = hoverCount.toString();
+    }
+}
+
+for(let i =0; i< (n*n); i++){
+    const box1 = document.createElement("div");
+    box1.className = "box1";
+    box1.style.cssText = "flex-basis:" + p + "%; ";
+    box1.addEventListener('mouseenter', handleHoverEnter);
+    fragment.append(box1);
+}
+
+box.append(fragment);
